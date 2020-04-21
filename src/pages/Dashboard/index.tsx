fab1d2de2611dbf98fc71571e3c1f490613bfee9
Igor Thierry <igorthierry15@gmail.com>
2020-04-21 14:59:51 -0300

    Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,81 @@
 import React from 'react';
-
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/16635562?s=460&v=4"
+            alt="Igor Thierry"
+          />
+          <div>
+            <strong>IgorThierry/github-explorer</strong>
+            <p>
+              Aplicação em ReactJS e TypeScript para explorar os repositórios do
+              GitHub através de sua API pública ;)
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/16635562?s=460&v=4"
+            alt="Igor Thierry"
+          />
+          <div>
+            <strong>IgorThierry/github-explorer</strong>
+            <p>
+              Aplicação em ReactJS e TypeScript para explorar os repositórios do
+              GitHub através de sua API pública ;)
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/16635562?s=460&v=4"
+            alt="Igor Thierry"
+          />
+          <div>
+            <strong>IgorThierry/github-explorer</strong>
+            <p>
+              Aplicação em ReactJS e TypeScript para explorar os repositórios do
+              GitHub através de sua API pública ;)
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/16635562?s=460&v=4"
+            alt="Igor Thierry"
+          />
+          <div>
+            <strong>IgorThierry/github-explorer</strong>
+            <p>
+              Aplicação em ReactJS e TypeScript para explorar os repositórios do
+              GitHub através de sua API pública ;)
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
