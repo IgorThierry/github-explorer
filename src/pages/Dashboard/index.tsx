6320e81d4840e6aa69dc3c1ef5c0c0ddf7a6ce4f
Igor Thierry <igorthierry15@gmail.com>
2020-04-21 23:20:39 -0300

    feat: page detail

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
-
 import { FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
@@ -19,21 +19,24 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
   const [inputError, setInputError] = useState('');
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const localRepos = localStorage.getItem('repositories');
-
-    if (localRepos) {
-      const repos = JSON.parse(localRepos);
-      setRepositories(repos);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('repositories', JSON.stringify(repositories));
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
   }, [repositories]);
 
   async function handleAddRepository(
@@ -82,7 +85,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -93,7 +99,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
-// import { Container } from './styles';
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} /> Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="Rocketseat"
+          />
+          <div>
+            <strong>{params.repository}</strong>
+            <p>Descrição do repo</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="teste">
+          <div>
+            <strong>repository full_name</strong>
+            <p>repository description</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
